525948c7d901385dddfc3240b9d55937e3360ef4
Mauro Salina <maurosalina85@gmail.com>
2025-01-08 14:37:28 -0300

    html de tasklist

---
+++ TaskList/script.js
@@ -0,0 +1,8 @@
+// Info date
+const dateNumber = document.getElementById('dateNumber');
+const dateText = document.getElementById('dateText');
+const dateMonth = document.getElementById('dateMonth');
+const dateYear = document.getElementById('dateYear');
+
+// task Container
+const taskContainer = document.getElementById('taskContainer');
