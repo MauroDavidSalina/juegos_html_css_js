66f4d68e31a9ec22427602ddc8f155707eaa1b0c
Mauro Salina <maurosalina85@gmail.com>
2025-01-09 09:21:24 -0300

    lista de tareas terminado

--- TaskList/script.js
+++ TaskList/script.js
@@ -5,4 +5,45 @@ const dateMonth = document.getElementById('dateMonth');
 const dateYear = document.getElementById('dateYear');
 
 // task Container
-const taskContainer = document.getElementById('taskContainer');
+const tasksContainer = document.getElementById('tasksContainer');
+
+const setDate = () => {
+    const date = new Date();
+    dateNumber.textContent = date.toLocaleString('es', {day: 'numeric'});
+    dateText.textContent = date.toLocaleString('es', {weekday: 'long'});
+    dateMonth.textContent = date.toLocaleString('es', {month: 'short'});
+    dateYear.textContent = date.toLocaleString('es', {year: 'numeric'});
+}
+
+const addNewTask = (event) => {
+    event.preventDefault();
+    const {value} = event.target.taskText;
+    if (value){
+        const task = document.createElement('div');
+        task.classList.add('task', 'roundBorder');
+        task.addEventListener('click', changeTaskState);
+        task.textContent = value;
+        tasksContainer.prepend(task);
+        event.target.reset();
+    }
+};
+
+const changeTaskState = (event) => {
+    event.target.classList.toggle('done');
+};
+
+const order = () => {
+    const done = [];
+    const toDo = [];
+    tasksContainer.childNodes.forEach( element => {
+        element.classList.contains('done') ? done.push(element) : toDo.push(element);
+    })
+    return [...toDo, ...done];
+};
+
+const renderOrderedTasks = () => {
+    order().forEach(element => tasksContainer.appendChild(element));
+};
+
+
+setDate();
